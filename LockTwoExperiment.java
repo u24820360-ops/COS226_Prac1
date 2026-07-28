1abe59374bc25ef82ee832e0f26f0c850f416eff
Azolile Mbanga <u24820360@tuks.co.za>
2026-07-28 17:01:58 +0200

    final commit hopefully

--- LockTwoExperiment.java
+++ LockTwoExperiment.java
@@ -1,7 +1,9 @@
 public class LockTwoExperiment {
-    public static class LockTwo implements Lock {
-        private long victim;
 
+    public static class LockTwo implements Lock {
+        private volatile long victim;
+
+        @Override
         public void lock() {
             if (Thread.currentThread().getName().endsWith("0")) {
                 victim = Thread.currentThread().getId();
@@ -9,58 +11,61 @@ public class LockTwoExperiment {
                 victim = Thread.currentThread().getId();
             }
 
-            while (victim == Thread.currentThread().getId()) {}
+            while (victim == Thread.currentThread().getId()) {
+            }
         }
 
+        @Override
         public void unlock() {}
+    }
 
-        public static class SharedCounter {
-            private int count = 0;
-            private LockTwo myLock;
-            public SharedCounter(LockTwo locktwo) {
-                this.myLock = locktwo;
+    public static class SharedCounter {
+        private int count = 0;
+        private LockTwo myLock;
+
+        public SharedCounter(LockTwo locktwo) {
+            this.myLock = locktwo;
+        }
+
+        public void increment() {
+            myLock.lock();
+            try {
+                count = count + 1;
+                System.out.println("Thread " + Thread.currentThread().getName() + " count is now on " + count);
+            } finally {
+                myLock.unlock();
             }
+        }
+    }
 
+    public static class CounterTask implements Runnable {
+        private SharedCounter savedCounter;
 
-            public void increment(){
-                myLock.lock();
+        public CounterTask(SharedCounter counter) {
+            this.savedCounter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 50; i++) {
+                savedCounter.increment();
                 try {
-                    count = count + 1;
-                    System.out.println("Thread " + Thread.currentThread().getName() + " count is now on " + count);
-                } finally {
-                    myLock.unlock();
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                    System.out.println("Thread Interrupted");
                 }
+                System.out.println("Task is running in: " + Thread.currentThread().getId());
             }
         }
+    }
 
-        public static class CounterTask implements Runnable {
-            private SharedCounter savedCounter;
-            public CounterTask(SharedCounter counter) {
-                this.savedCounter = counter;
-            }
-
-            @Override
-            public void run() {
-                for (int i = 0; i < 5; i++) {
-                    savedCounter.increment();
-                    try {
-                        Thread.sleep(500);
-                    } catch (Exception e) {
-                        System.out.println("Thread Interrupted");
-                    }
-                    System.out.println("Task is running in: " + Thread.currentThread().getId());
-                }
-            }
-        }
-
-        public static void main(String[] args) {
-            LockTwo lt = new LockTwo();
-            SharedCounter sc = new SharedCounter(lt);
-            CounterTask ct = new CounterTask(sc);
-            Thread thrOne = new Thread(ct, "Thread-0");
-            Thread thrTwo = new Thread(ct, "Thread-1");
-            thrOne.start();
-            thrTwo.start();
-        }
+    public static void main(String[] args) {
+        LockTwo lt = new LockTwo();
+        SharedCounter sc = new SharedCounter(lt);
+        CounterTask ct = new CounterTask(sc);
+        Thread thrOne = new Thread(ct, "Thread-0");
+        Thread thrTwo = new Thread(ct, "Thread-1");
+        thrOne.start();
+        thrTwo.start();
     }
 }
